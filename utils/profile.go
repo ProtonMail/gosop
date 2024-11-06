5212037f43240a023d9a5e363f48d8c6f5e595b2
Lukas Burkhalter <lukas.burkhalter@proton.ch>
2024-11-06 10:05:42 +0100

    feat:Use GopenPGP preset profiles

--- utils/profile.go
+++ utils/profile.go
@@ -1,12 +1,8 @@
 package utils
 
 import (
-	"crypto"
 	"strings"
 
-	"github.com/ProtonMail/go-crypto/openpgp/packet"
-	"github.com/ProtonMail/go-crypto/openpgp/s2k"
-	"github.com/ProtonMail/gopenpgp/v3/constants"
 	"github.com/ProtonMail/gopenpgp/v3/profile"
 )
 
@@ -79,68 +75,13 @@ func createKeyGenerationProfiles() []*SopProfile {
 }
 
 func defaultProfile() *profile.Custom {
-	setKeyAlgorithm := func(cfg *packet.Config, securityLevel int8) {
-		cfg.Algorithm = packet.PubKeyAlgoEdDSA
-		switch securityLevel {
-		case constants.HighSecurity:
-			cfg.Curve = packet.Curve25519
-		default:
-			cfg.Curve = packet.Curve25519
-		}
-	}
-	return &profile.Custom{
-		SetKeyAlgorithm:      setKeyAlgorithm,
-		Hash:                 crypto.SHA256,
-		CipherEncryption:     packet.CipherAES256,
-		CompressionAlgorithm: packet.CompressionZLIB,
-		CompressionConfiguration: &packet.CompressionConfig{
-			Level: 6,
-		},
-	}
+	return profile.Default()
 }
 
 func rfc4880() *profile.Custom {
-	setKeyAlgorithm := func(cfg *packet.Config, securityLevel int8) {
-		cfg.Algorithm = packet.PubKeyAlgoRSA
-		switch securityLevel {
-		case constants.HighSecurity:
-			cfg.RSABits = 4096
-		default:
-			cfg.RSABits = 3072
-		}
-	}
-	return &profile.Custom{
-		SetKeyAlgorithm:      setKeyAlgorithm,
-		Hash:                 crypto.SHA256,
-		CipherEncryption:     packet.CipherAES256,
-		CompressionAlgorithm: packet.CompressionZLIB,
-	}
+	return profile.RFC4880()
 }
 
 func rfc9580() *profile.Custom {
-	setKeyAlgorithm := func(cfg *packet.Config, securityLevel int8) {
-		switch securityLevel {
-		case constants.HighSecurity:
-			cfg.Algorithm = packet.PubKeyAlgoEd448
-		default:
-			cfg.Algorithm = packet.PubKeyAlgoEd25519
-		}
-	}
-	return &profile.Custom{
-		SetKeyAlgorithm:      setKeyAlgorithm,
-		Hash:                 crypto.SHA512,
-		CipherEncryption:     packet.CipherAES256,
-		CompressionAlgorithm: packet.CompressionZLIB,
-		AeadKeyEncryption:    &packet.AEADConfig{},
-		AeadEncryption:       &packet.AEADConfig{},
-		S2kKeyEncryption: &s2k.Config{
-			S2KMode:      s2k.Argon2S2K,
-			Argon2Config: &s2k.Argon2Config{},
-		},
-		S2kEncryption: &s2k.Config{
-			S2KMode:      s2k.Argon2S2K,
-			Argon2Config: &s2k.Argon2Config{},
-		},
-		V6: true,
-	}
+	return profile.RFC9580()
 }
